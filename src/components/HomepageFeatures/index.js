e61db28108b3fc384ec8840cbf16b7053ed94817
Ivan Herrera <pertuz27@hotmail.com>
2022-06-13 11:24:37 -0500

    agregadas las citas

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -8,7 +8,8 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        La ciencia de los datos está cambiando la forma en que los agricultores y los profesionales de la agricultura han estado tomando decisiones <a href='https://towardsdatascience.com/6-ways-the-agricultural-industry-is-benefiting-from-data-scientists-b778d83f61db' target="_blank">(Matthews, 2019)</a>.
+        La ciencia de los datos está cambiando la forma en que los agricultores y los profesionales de la agricultura han estado tomando decisiones 
+        <a href='https://towardsdatascience.com/6-ways-the-agricultural-industry-is-benefiting-from-data-scientists-b778d83f61db' target="_blank"> (Matthews, 2019)</a>.
       </>
     ),
   },
@@ -17,7 +18,8 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        La tecnología moderna ha permitido recopilar datos del suelo, el agua y los minerales de las explotaciones agrícolas, y almacenarlos en un sistema centralizado, conocido popularmente como Internet de las Cosas (IoT). IoT se refiere a la idea de conectar a Internet dispositivos interrelacionados para que puedan compartir e intercambiar datos de forma independiente (Clark, 2016). 
+        La tecnología moderna ha permitido recopilar datos del suelo, el agua y los minerales de las explotaciones agrícolas, y almacenarlos en un sistema centralizado, conocido popularmente como Internet de las Cosas (IoT). IoT se refiere a la idea de conectar a Internet dispositivos interrelacionados para que puedan compartir e intercambiar datos de forma independiente 
+        <a href='https://www.ibm.com/blogs/internet-of-things/what-is-the-iot/' target="_blank"> (Clark, 2016)</a>. 
       </>
     ),
   },
@@ -26,7 +28,8 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        La analítica de datos puede utilizarse en el volumen acumulado para obtener información que los agricultores pueden utilizar para optimizar su agricultura. Así, los agricultores pueden tomar decisiones agrícolas inteligentes utilizando esa información a lo largo del ciclo de producción; desde la planificación, la plantación, la cosecha, hasta su comercialización (Maru, 2018).
+        La analítica de datos puede utilizarse en el volumen acumulado para obtener información que los agricultores pueden utilizar para optimizar su agricultura. Así, los agricultores pueden tomar decisiones agrícolas inteligentes utilizando esa información a lo largo del ciclo de producción; desde la planificación, la plantación, la cosecha, hasta su comercialización 
+        <a href='https://cgspace.cgiar.org/bitstream/handle/10568/92477/GFAR-GODAN-CTA-white-paper-final.pdf' target="_blank"> (Maru, 2018).</a>
       </>
     ),
   },
